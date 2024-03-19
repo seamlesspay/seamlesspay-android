4153f3681219cf8927604ec4967518a04fd2d775
Michael <undermyclouds@gmail.com>
2024-03-19 20:04:44 +0200

    Refactor baseline package structure

---
+++ sdk/src/main/java/com/seamlesspay/ui/common/CardInputListener.kt
@@ -0,0 +1,50 @@
+package com.seamlesspay.ui.common
+
+import androidx.annotation.StringDef
+
+/**
+ * Represents a listener for card input events. Note that events are
+ * not one-time events. For instance, a user can "complete" the CVC many times
+ * by deleting and re-entering the value.
+ */
+interface CardInputListener {
+
+    @Retention(AnnotationRetention.SOURCE)
+    @StringDef(FocusField.FOCUS_CARD, FocusField.FOCUS_EXPIRY, FocusField.FOCUS_CVC,
+            FocusField.FOCUS_POSTAL)
+    annotation class FocusField {
+        companion object {
+            const val FOCUS_CARD: String = "focus_card"
+            const val FOCUS_EXPIRY: String = "focus_expiry"
+            const val FOCUS_CVC: String = "focus_cvc"
+            const val FOCUS_POSTAL: String = "focus_postal"
+        }
+    }
+
+    /**
+     * Called whenever the field of focus within the widget changes.
+     *
+     * @param focusField a [FocusField] to which the focus has just changed.
+     */
+    fun onFocusChange(@FocusField focusField: String)
+
+    /**
+     * Called when a potentially valid card number has been completed in the
+     * [CardNumberEditText]. May be called multiple times if the user edits
+     * the field.
+     */
+    fun onCardComplete()
+
+    /**
+     * Called when a expiration date (one that has not yet passed) has been entered.
+     * May be called multiple times, if the user edits the date.
+     */
+    fun onExpirationComplete()
+
+    /**
+     * Called when a potentially valid CVC has been entered. The only verification performed
+     * on the number is that it is the correct length. May be called multiple times, if
+     * the user edits the CVC.
+     */
+    fun onCvcComplete()
+}
---
+++ sdk/src/main/java/com/seamlesspay/ui/utils/SeamlesspayTextUtils.kt
@@ -0,0 +1,53 @@
+package com.seamlesspay.ui
+
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.Locale
+
+/**
+ * Utility class for common text-related operations on Seamlesspay data coming from the server.
+ */
+object SeamlesspayTextUtils {
+
+    /**
+     * Converts a card number that may have spaces between the numbers into one without any spaces.
+     * Note: method does not check that all characters are digits or spaces.
+     *
+     * @param cardNumberWithSpaces a card number, for instance "4242 4242 4242 4242"
+     * @return the input number minus any spaces, for instance "4242424242424242".
+     * Returns `null` if the input was `null` or all spaces.
+     */
+    @JvmStatic
+    fun removeSpacesAndHyphens(cardNumberWithSpaces: String?): String? {
+        return cardNumberWithSpaces.takeUnless { it.isNullOrBlank() }
+            ?.replace("\\s|-".toRegex(), "")
+    }
+
+    /**
+     * Calculate a hash value of a String input and convert the result to a hex string.
+     *
+     * @param toHash a value to hash
+     * @return a hexadecimal string
+     */
+    internal fun shaHashInput(toHash: String?): String? {
+        if (toHash.isNullOrBlank()) {
+            return null
+        }
+
+        return try {
+            val digest = MessageDigest.getInstance("SHA-1")
+            val bytes = toHash.toByteArray(StandardCharsets.UTF_8)
+            digest.update(bytes, 0, bytes.size)
+            bytesToHex(digest.digest())
+        } catch (noSuchAlgorithm: NoSuchAlgorithmException) {
+            null
+        }
+    }
+
+    private fun bytesToHex(bytes: ByteArray): String {
+        return bytes
+            .joinToString("") { String.format(Locale.ROOT, "%02x", it) }
+            .toUpperCase(Locale.ROOT)
+    }
+}
